5c2d055b1a852fd83fb9e6bc8cf77b38e55919b0
Leander Janssen <leanderjanssen@users.noreply.github.com>
2022-02-10 09:33:41 +0100

    Update remote_backend.tf

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "nuaware-instruqt-2021-11"
+    organization = "nuaware-instruqt"
     workspaces {
       name = "hashicat-aws"
     }
